5c69ae3866af5c3cce93eb3e2c6061dcf73e20f8
nagerz <znager@gmail.com>
2019-07-07 14:16:31 -0600

    Adds SFC navbar alternative

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill babdge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Funcitonal Component Option (shortcut for creating = 'sfc' + tab)
+
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill babdge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+// Class componant (shortcut for creating = 'cc' + tab)
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar{" "}
+//           <span className="badge badge-pill babdge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
 
 export default NavBar;
