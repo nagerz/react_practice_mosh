3806cbee0c8749b0958e163f82f08537260acfe5
nagerz <znager@gmail.com>
2019-07-07 14:20:19 -0600

    Adds object destructuring to navbar and counters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 // Stateless Funcitonal Component Option (shortcut for creating = 'sfc' + tab)
 
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill babdge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
